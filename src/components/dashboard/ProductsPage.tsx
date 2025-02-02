abf61e0c5d717d253433315bdc631ab1a622486b
Muhammad Ubaid <39967022+muhammadubaid001@users.noreply.github.com>
2025-02-02 13:43:15 +0500

    Window undefined error

--- src/components/dashboard/ProductsPage.tsx
+++ src/components/dashboard/ProductsPage.tsx
@@ -6,16 +6,16 @@ export const ProductsPage = () => {
     return (
         <div className="bg-sidebar rounded-2xl mt-4 p-6 max-w-xl mx-auto">
             <p className="font-semibold text-2xl text-center">Makers</p>
-            <p className="text-sm text-center text-gray-300 w-96 mt-2 mx-auto">Generate maker volume on Dexscreener to help boost your trending score. For only 0.0005 SOL per maker (minimum of 10 makers required)</p>
+            <p className="text-sm text-center text-gray-300 md:w-96 mt-2 mx-auto">Generate maker volume on Dexscreener to help boost your trending score. For only 0.0005 SOL per maker (minimum of 10 makers required)</p>
             <form>
-                <div className="flex gap-4 mt-8">
+                <div className="flex flex-col md:flex-row   gap-4 mt-8">
                     <div className="flex-1 flex relative flex-col gap-3">
                         <label className="text-sm text-gray-300">Token Address</label>
                         <input type="text" placeholder="Mint Contact Address" className="placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
                         <Star className="absolute h-4 w-4 left-4 top-11" />
                     </div>
                     <div className="flex-1 flex relative flex-col gap-3">
-                        <label className="text-sm text-gray-300">Number of Makers (Min 10 Max 1000)</label>
+                        <label className="text-sm text-gray-300 whitespace-nowrap">Number of Makers (Min 10 Max 1000)</label>
                         <input type="text" placeholder="Markers required" className="placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
                         <IconExclamationCircle className="absolute h-4 w-4 left-4 top-11" />
                     </div>
--- src/components/dashboard/TasksPage.tsx
+++ src/components/dashboard/TasksPage.tsx
@@ -6,23 +6,23 @@ export const TasksPage = () => {
     return (
         <div className="bg-sidebar rounded-2xl p-6 mt-4 max-w-xl mx-auto">
             <p className="font-semibold text-2xl text-center">Token Holders</p>
-            <p className="text-sm text-center text-gray-300 w-96 mt-2 mx-auto">Fetch a list of token holders for a particular token. Just input the contract address to start.
+            <p className="text-sm text-center text-gray-300 md:w-96 mt-2 mx-auto">Fetch a list of token holders for a particular token. Just input the contract address to start.
             ‍</p>
             <form>
-                <div className="grid grid-cols-2 gap-4 mt-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
                     <div className=" flex relative flex-col gap-3">
                         <label className="text-sm text-gray-300">Token Address</label>
-                        <input type="text" placeholder="Mint Contact Address" className="placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
+                        <input type="text" placeholder="Mint Contact Address" className="pr-3 placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
                         <Star className="absolute h-4 w-4 left-4 top-11" />
                     </div>
                     <div className=" flex relative flex-col gap-3">
-                        <label className="text-sm text-gray-300">Number of Makers (Min 10 Max 1000)</label>
-                        <input type="text" placeholder="Markers required" className="placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
+                        <label className="text-sm text-gray-300 ">Number of Makers (Min 10 Max 1000)</label>
+                        <input type="text" placeholder="Markers required" className="pr-3 placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
                         <IconExclamationCircle className="absolute h-4 w-4 left-4 top-11" />
                     </div>
                     <div className=" flex relative flex-col gap-3">
                         <label className="text-sm text-gray-300">Offset</label>
-                        <input type="text" placeholder="Offset Amount" className="placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
+                        <input type="text" placeholder="Offset Amount" className="pr-3 placeholder:font-light placeholder:text-sm rounded-full border-0 focus:border focus:border-white focus:ring-0 focus:outline-none bg-[#151B2D] pl-10 py-2" />
                         <IconExclamationCircle className="absolute h-4 w-4 left-4 top-11" />
                     </div>
                 </div>
